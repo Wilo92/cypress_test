e0f74331f241fd6bccb0f451369e7a81095bdba8
jose nicolas aristizabal ramirez <josenicolasaristizabalramirez@gmail.com>
2025-10-30 19:35:15 -0500

    feat: add test for go to cart step

---
+++ cypress/e2e/03-go_to_cart/go_to_cart.cy.js
@@ -0,0 +1,56 @@
+/// <reference types="cypress" />
+import users from "../../fixtures/users.json";
+import devices from "../../fixtures/devices.json";
+import ProductsPage from "../../pages/ProductsPage.js";
+import CartPage from "../../pages/CartPage.js";
+
+describe("Pruebas de ir al carrito - multi dispositivo", () => {
+  const urlHome = Cypress.env("urlHome");
+  let productsPage;
+  let cartPage;
+
+  beforeEach(() => {
+    productsPage = new ProductsPage();
+    cartPage = new CartPage();
+  });
+
+  devices.forEach(({ name, width, height }) => {
+    describe(`Pruebas en ${name}`, () => {
+      beforeEach(() => {
+        cy.viewport(width, height);
+        cy.login(users.standard.username, users.standard.password);
+        productsPage.verifyPageLoaded();
+      });
+
+      it(`Agregar producto y navegar al carrito en ${name}`, () => {
+        const productName = "Sauce Labs Backpack";
+
+        productsPage
+          .addProductToCart(productName)
+          .goToCart();
+
+        cartPage
+          .verifyPageLoaded()
+          .verifyProductInCart(productName);
+      });
+
+      it(`Agregar múltiples productos y verificar en carrito en ${name}`, () => {
+        const productNames = [
+          "Sauce Labs Backpack",
+          "Sauce Labs Bike Light",
+          "Sauce Labs Bolt T-Shirt"
+        ];
+
+        productsPage
+          .addMultipleProductsToCart(productNames)
+          .goToCart();
+
+        cartPage
+          .verifyPageLoaded()
+          .verifyMultipleProductsInCart(productNames)
+          .verifyCartItemsCount(productNames.length);
+      });
+    });
+  });
+});
+
